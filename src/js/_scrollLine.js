68ef619d8d7b661587f260f93e154f6006ba75b9
Elena <tcachelena@gmail.com>
2021-03-22 10:49:53 +0300

    .

--- src/js/_scrollLine.js
+++ src/js/_scrollLine.js
@@ -1,57 +1,75 @@
 'use strict';
 
-
-const progressEl = document.querySelector(`.js-progress`);
-const article = document.querySelectorAll(`.article__content`);
+document.addEventListener('DOMContentLoaded', () => {
 
 
-window.addEventListener(`scroll`, function () {
-    onProgressScroll(progressEl);
+    const headerFixed = document.querySelector(`.js-header`);
+    const scroll = 0;
+
+    const progressEl = document.querySelector(`.js-progress`);
+    const article = document.querySelectorAll(`.article__content`);
+    let scrollTop = document.scrollTop;
+
+
+
+    window.addEventListener(`scroll`, function () {
+        onProgressScroll(progressEl);
+
+        scrollClient()
+
+    })
+    scrollClient()
+    function scrollClient() {
+        if (scrollTop > scroll) {
+            headerFixed.classList.add(`fixed`);
+        } else {
+            headerFixed.classList.remove(`fixed`);
+        }
+    }
+
+
+
+    // const articleParallax = function () {
+    //     article.forEach((item) => {
+    //         item.style.top = value + 5 + `px`;
+    //         console.log(`efasfd`)
+    //     })
+
+    // }
+
+    // articleParallax();
+
+    const onProgressScroll = function (element) {
+
+        let windowHeight = window.innerHeight;
+        let siteHeight = document.documentElement.scrollHeight;
+
+        let percentageProgress = Math.floor(scrollTop / (siteHeight - windowHeight) * 100);
+
+        element.style.height = `${percentageProgress}%`;
+    }
+
+
+
+    const pos = document.documentElement;
+    pos.addEventListener(`mousemove`, e => {
+        pos.style.setProperty(`--x`, e.clientX + `px`);
+        pos.style.setProperty(`--y`, e.clientY + `px`);
+    })
+
+
+
+    // const onProgressScroll = (element) => {
+    //     const windowClient = document.documentElement.scrollTop;
+    //     const windowHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    //     const scrollPercent = (windowClient / windowHeight) * 100;
+
+    //     element.style.width = scrollPercent + `%`;
+    // }
+
+    // window.addEventListener(`scroll`, () => onProgressScroll(progressEl));
+
+
+
 
 })
-
-
-
-// const articleParallax = function () {
-//     article.forEach((item) => {
-//         item.style.top = value + 5 + `px`;
-//         console.log(`efasfd`)
-//     })
-
-// }
-
-// articleParallax();
-
-const onProgressScroll = function (element) {
-    let scrollTop = window.scrollY;
-    let windowHeight = window.innerHeight;
-    let siteHeight = document.documentElement.scrollHeight;
-
-    let percentageProgress = Math.floor(scrollTop / (siteHeight - windowHeight) * 100);
-
-    element.style.height = `${percentageProgress}%`;
-}
-
-
-
-const pos = document.documentElement;
-pos.addEventListener(`mousemove`, e => {
-    pos.style.setProperty(`--x`, e.clientX + `px`);
-    pos.style.setProperty(`--y`, e.clientY + `px`);
-})
-
-
-
-// const onProgressScroll = (element) => {
-//     const windowClient = document.documentElement.scrollTop;
-//     const windowHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-//     const scrollPercent = (windowClient / windowHeight) * 100;
-
-//     element.style.width = scrollPercent + `%`;
-// }
-
-// window.addEventListener(`scroll`, () => onProgressScroll(progressEl));
-
-
-
-
