b5538647c063380ad6381074a48ca7be49ce220c
Elena <tcachelena@gmail.com>
2021-04-20 15:18:51 +0300

    added meta

--- src/js/_parallax.js
+++ src/js/_parallax.js
@@ -1,6 +1,6 @@
 
 const images = document.querySelectorAll('.js-parallax');
-console.log(images);
+
 new simpleParallax(images, {
     scale: 1.2,
     delay: .6,
--- src/js/_proba.js
+++
@@ -1,7 +0,0 @@
-const names = document.querySelector(`.js-name`);
-const a = names[0];
-
-// const str = names.innerText;
-// const symbol = str.split('');
-
-console.log(a);
